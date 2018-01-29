6b5098525bf083f4e5225510370d52582ebc9340
okayohyeah <ohkathy@gmail.com>
2018-01-28 20:57:35 -0800

    update README, change text in App.js and index.css

--- README.md
+++ README.md
@@ -6,4 +6,14 @@ A place for substitute teachers to track and organize assignments.
 * A way to filter past assignments quickly because once assignments are posted, one has to decide immediately. 
 * This app offsets the time pressure.
 
-[Write once, run anywhere with Create React (Native) App and react-native-web by Yannick Spark](https://sparkyspace.com/write-once-run-anywhere-with-create-react-native-app-and-react-native-web/#comment-3688417951)
+## Development
+* Have Xcode installed on Mac.
+* In Terminal 1: Web App
+<code>yarn start-web </code>
+* In Terminal 2: Mobile App
+<code>yarn ios</code>
+
+### Write code as in React Native code
+This Hybrid App runs on the Web, iOS and Android
+
+[Write once, run anywhere with Create React (Native) App and react-native-web by @Yannick Spark](https://sparkyspace.com/write-once-run-anywhere-with-create-react-native-app-and-react-native-web/#comment-3688417951)
--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       <View style={styles.app}>
+        <View style={styles.appHeader}>
+          <Text style={styles.appTitle}><img src={logo} className="App-logo" alt="logo" /> Subhub <img src={logo} className="App-logo" alt="logo" /></Text>
+        </View>
+        <Text style={styles.appIntro}>
+          The center for subs
+        </Text>
+      </View>
     );
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  app: {
+    flex: 1
+  },
+  appHeader: {
+    flex: 1,
+    backgroundColor: '#222',
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  appTitle: {
+    fontSize: 100,
+    color: 'white'
+  },
+  appIntro: {
+    flex: 2,
+    fontSize: 30,
+    textAlign: 'center'
+  }
+})
+
